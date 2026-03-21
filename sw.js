86b4a707b14b75cbd4cce7d0392daf4481cddeeb
nook-restaurant <mikael.godineau@hotmail.fr>
2026-03-21 13:47:27 +0100

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,33 @@
+const CACHE = 'nook-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  './img/logo.png',
+  './img/facade.jpg',
+  './img/curry.jpg',
+  './img/dessert.jpg',
+  './img/assiette.jpg',
+  './img/restaurant.jpg'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE).then(c => c.addAll(ASSETS))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(cached => cached || fetch(e.request))
+  );
+});
